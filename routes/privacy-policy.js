9884af2bf5fc35819718ca5e48dafda24f6a3d36
Анатолий <contact@anafro.ru>
2022-04-05 13:31:33 +1000

    Added title to document pages

--- routes/privacy-policy.js
+++ routes/privacy-policy.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('privacy-policy');
+  res.render('privacy-policy', { 
+    title: 'Website Privacy Policy' 
+  });
 });
 
 module.exports = router;
--- routes/terms-and-conditions-of-use.js
+++ routes/terms-and-conditions-of-use.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('terms-of-use');
+  res.render('terms-of-use', { title: 'Website Terms and Conditions of Use' });
 });
 
 module.exports = router;
