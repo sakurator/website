f7c5a3e470fd540e1b19caa99f44b4121e1ba176
Анатолий <contact@anafro.ru>
2022-04-15 12:59:20 +1000

    Added IDs

--- prisma/data/letters.js
+++ prisma/data/letters.js
@@ -1,193 +1,239 @@
 module.exports = [
     {
+        id: 1,
         name: 'a',
         row_id: 1,
     },
     {
+        id: 2,
         name: 'i',
         row_id: 1,
     },
     {
+        id: 3,
         name: 'u',
         row_id: 1,
     },
     {
+        id: 4,
         name: 'e',
         row_id: 1,
     },
     {
+        id: 5,
         name: 'o',
         row_id: 1,
     },
     ////////////////////////////////////
     {
+        id: 6,
         name: 'ka',
         row_id: 2,
     },
     {
+        id: 7,
         name: 'ki',
         row_id: 2,
     },
     {
+        id: 8,
         name: 'ku',
         row_id: 2,
     },
     {
+        id: 9,
         name: 'ke',
         row_id: 2,
     },
     {
+        id: 10,
         name: 'ko',
         row_id: 2,
     },
     ////////////////////////////////////
     {
+        id: 11,
         name: 'sa',
         row_id: 3,
     },
     {
+        id: 12,
         name: 'shi',
         row_id: 3,
     },
     {
+        id: 13,
         name: 'su',
         row_id: 3,
     },
     {
+        id: 14,
         name: 'se',
         row_id: 3,
     },
     {
+        id: 15,
         name: 'so',
         row_id: 3,
     },
     ////////////////////////////////////
     {
+        id: 16,
         name: 'ta',
         row_id: 4,
     },
     {
+        id: 17,
         name: 'chi',
         row_id: 4,
     },
     {
+        id: 18,
         name: 'tsu',
         row_id: 4,
     },
     {
+        id: 19,
         name: 'te',
         row_id: 4,
     },
     {
+        id: 20,
         name: 'to',
         row_id: 4,
     },
     ////////////////////////////////////
     {
+        id: 21,
         name: 'na',
         row_id: 5,
     },
     {
+        id: 22,
         name: 'ni',
         row_id: 5,
     },
     {
+        id: 23,
         name: 'nu',
         row_id: 5,
     },
     {
+        id: 24,
         name: 'ne',
         row_id: 5,
     },
     {
+        id: 25,
         name: 'no',
         row_id: 5,
     },
     ////////////////////////////////////
     {
+        id: 26,
         name: 'ha',
         row_id: 6,
     },
     {
+        id: 27,
         name: 'hi',
         row_id: 6,
     },
     {
+        id: 28,
         name: 'fu',
         row_id: 6,
     },
     {
+        id: 29,
         name: 'he',
         row_id: 6,
     },
     {
+        id: 30,
         name: 'ho',
         row_id: 6,
     },
     ////////////////////////////////////
     {
+        id: 31,
         name: 'ma',
         row_id: 7,
     },
     {
+        id: 32,
         name: 'mi',
         row_id: 7,
     },
     {
+        id: 33,
         name: 'mu',
         row_id: 7,
     },
     {
+        id: 34,
         name: 'me',
         row_id: 7,
     },
     {
+        id: 35,
         name: 'mo',
         row_id: 7,
     },
     ////////////////////////////////////
     {
+        id: 36,
         name: 'ra',
         row_id: 8,
     },
     {
+        id: 37,
         name: 'ri',
         row_id: 8,
     },
     {
+        id: 38,
         name: 'ru',
         row_id: 8,
     },
     {
+        id: 39,
         name: 're',
         row_id: 8,
     },
     {
+        id: 40,
         name: 'ro',
         row_id: 8,
     },
     ////////////////////////////////////
     {
+        id: 41,
         name: 'ya',
         row_id: 9,
     },
     {
+        id: 42,
         name: 'yu',
         row_id: 9,
     },
     {
+        id: 43,
         name: 'yo',
         row_id: 9,
     },
     {
+        id: 44,
         name: 'wa',
         row_id: 9,
     },
     {
+        id: 45,
         name: 'wo',
         row_id: 9,
     },
     {
+        id: 46,
         name: 'n',
         row_id: 9,
     },
@@ -195,194 +241,240 @@ module.exports = [
     ////////////////////////////////////
     ////////////////////////////////////
     {
+        id: 47,
         name: 'a',
         row_id: 10,
     },
     {
+        id: 48,
         name: 'i',
         row_id: 10,
     },
     {
+        id: 49,
         name: 'u',
         row_id: 10,
     },
     {
+        id: 50,
         name: 'e',
         row_id: 10,
     },
     {
+        id: 51,
         name: 'o',
         row_id: 10,
     },
     ////////////////////////////////////
     {
+        id: 52,
         name: 'ka',
         row_id: 11,
     },
     {
+        id: 53,
         name: 'ki',
         row_id: 11,
     },
     {
+        id: 54,
         name: 'ku',
         row_id: 11,
     },
     {
+        id: 55,
         name: 'ke',
         row_id: 11,
     },
     {
+        id: 56,
         name: 'ko',
         row_id: 11,
     },
     ////////////////////////////////////
     {
+        id: 57,
         name: 'sa',
         row_id: 12,
     },
     {
+        id: 58,
         name: 'shi',
         row_id: 12,
     },
     {
+        id: 59,
         name: 'su',
         row_id: 12,
     },
     {
+        id: 60,
         name: 'se',
         row_id: 12,
     },
     {
+        id: 61,
         name: 'so',
         row_id: 12,
     },
     ////////////////////////////////////
     {
+        id: 62,
         name: 'ta',
         row_id: 13,
     },
     {
+        id: 63,
         name: 'chi',
         row_id: 13,
     },
     {
+        id: 64,
         name: 'tsu',
         row_id: 13,
     },
     {
+        id: 65,
         name: 'te',
         row_id: 13,
     },
     {
+        id: 66,
         name: 'to',
         row_id: 13,
     },
     ////////////////////////////////////
     {
+        id: 67,
         name: 'na',
         row_id: 14,
     },
     {
+        id: 68,
         name: 'ni',
         row_id: 14,
     },
     {
+        id: 69,
         name: 'nu',
         row_id: 14,
     },
     {
+        id: 70,
         name: 'ne',
         row_id: 14,
     },
     {
+        id: 71,
         name: 'no',
         row_id: 14,
     },
     ////////////////////////////////////
     {
+        id: 72,
         name: 'ha',
         row_id: 15,
     },
     {
+        id: 73,
         name: 'hi',
         row_id: 15,
     },
     {
+        id: 74,
         name: 'fu',
         row_id: 15,
     },
     {
+        id: 75,
         name: 'he',
         row_id: 15,
     },
     {
+        id: 76,
         name: 'ho',
         row_id: 15,
     },
     ////////////////////////////////////
     {
+        id: 77,
         name: 'ma',
         row_id: 16,
     },
     {
+        id: 78,
         name: 'mi',
         row_id: 16,
     },
     {
+        id: 79,
         name: 'mu',
         row_id: 16,
     },
     {
+        id: 80,
         name: 'me',
         row_id: 16,
     },
     {
+        id: 81,
         name: 'mo',
         row_id: 16,
     },
     ////////////////////////////////////
     {
+        id: 82,
         name: 'ra',
         row_id: 17,
     },
     {
+        id: 83,
         name: 'ri',
         row_id: 17,
     },
     {
+        id: 84,
         name: 'ru',
         row_id: 17,
     },
     {
+        id: 85,
         name: 're',
         row_id: 17,
     },
     {
+        id: 86,
         name: 'ro',
         row_id: 17,
     },
     ////////////////////////////////////
     {
+        id: 87,
         name: 'ya',
         row_id: 18,
     },
     {
+        id: 88,
         name: 'yu',
         row_id: 18,
     },
     {
+        id: 89,
         name: 'yo',
         row_id: 18,
     },
     {
+        id: 90,
         name: 'wa',
         row_id: 18,
     },
     {
+        id: 91,
         name: 'wo',
         row_id: 18,
     },
     {
+        id: 92,
         name: 'n',
         row_id: 18,
     },
