c4d5118459b86c26284585c0b46016193d416e44
Anatoly Frolov <contact@anafro.ru>
2023-11-26 13:58:46 +1000

    Change particle & Add progress reset & Format

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,102 +1,205 @@
-let currentAlphabet = "hiragana"
+let currentAlphabet = "hiragana";
+const contentContainerEl = document.getElementById("content-container");
+const progressResetContainerEl = document.getElementById(
+	"progress-reset-container"
+);
 
 function updatePetalScores() {
-    document.querySelectorAll('.row-subtitle').forEach(subtitle => {
-        const petalsEarned = parseInt(getCookie(currentAlphabet + "_" + subtitle.id.split('-')[1]))
-        const petalsToComplete = parseInt(subtitle.id.split('-')[2])
-        subtitle.innerHTML = petalsEarned + (petalsEarned >= petalsToComplete ? '' : ` / ${petalsToComplete}`) + ' petals'
-    })
+	document.querySelectorAll(".row-subtitle").forEach((subtitle) => {
+		const petalsEarned = parseInt(
+			getCookie(currentAlphabet + "_" + subtitle.id.split("-")[1], 0)
+		);
+		const petalsToComplete = parseInt(subtitle.id.split("-")[2]);
+		const setCompleted = petalsEarned >= petalsToComplete;
+		subtitle.innerHTML =
+			petalsEarned +
+			(setCompleted ? "" : ` / ${petalsToComplete}`) +
+			`<img src="/images/particles/${
+				currentAlphabet === "hiragana" ? "petal" : "leaf"
+			}_${randomInt(PETAL_PARTICLE_VARIATIONS) + 1}.svg"/>`;
+	});
 
-    document.querySelectorAll('.row-progress-full').forEach(progressBar => {
-        const petalsEarned = parseInt(getCookie(currentAlphabet + "_" + progressBar.children[0].id.split('-')[1]))
-        const petalsToComplete = parseInt(progressBar.children[0].id.split('-')[2])
-        const ratio = petalsEarned / petalsToComplete;
+	document.querySelectorAll(".row-progress-full").forEach((progressBar) => {
+		const petalsEarned = parseInt(
+			getCookie(
+				currentAlphabet +
+					"_" +
+					progressBar.children[0].id.split("-")[1],
+				0
+			)
+		);
+		const petalsToComplete = parseInt(
+			progressBar.children[0].id.split("-")[2]
+		);
+		const ratio = petalsEarned / petalsToComplete;
 
-        if(ratio < 1) {
-            progressBar.style.display = 'flex'
-            progressBar.children[0].style.width = `${ratio * 100}%`
-        } else {
-            progressBar.style.display = 'none'
-        }
-    })
+		if (ratio < 1) {
+			progressBar.style.display = "flex";
+			progressBar.children[0].style.width = `${ratio * 100}%`;
+		} else {
+			progressBar.style.display = "none";
+		}
+	});
 
-    document.querySelectorAll('.row-completed-icon').forEach(icon => {
-        const petalsEarned = parseInt(getCookie(currentAlphabet + "_" + icon.id.split('-')[1]))
-        const petalsToComplete = parseInt(icon.id.split('-')[2])
+	document.querySelectorAll(".row-completed-icon").forEach((icon) => {
+		const petalsEarned = parseInt(
+			getCookie(currentAlphabet + "_" + icon.id.split("-")[1])
+		);
+		const petalsToComplete = parseInt(icon.id.split("-")[2]);
 
-        icon.style.display = petalsEarned >= petalsToComplete ? 'unset' : 'none'
-    })
+		icon.style.display =
+			petalsEarned >= petalsToComplete ? "unset" : "none";
+	});
 }
 
 /**************************************************************************************************
  *                                        Alphabet switch                                         *
  **************************************************************************************************/
-document.getElementById("alphabet-switch").addEventListener('click', event => {
-    const switchCheckbox = document.getElementById("alphabet-switch-checkbox")
-    const hiraganaTitle = document.getElementById("hiragana-alphabet-switch-title")
-    const katakanaTitle = document.getElementById("katakana-alphabet-switch-title")
-    const kanaImages = document.querySelectorAll(".row-kana")
-    const kanaProgressBars = document.querySelectorAll(".row-progress-completed")
+const alphabetSwitchEl = document.getElementById("alphabet-switch");
+const switchCheckboxEl = document.getElementById("alphabet-switch-checkbox");
+const hiraganaTitleEl = document.getElementById(
+	"hiragana-alphabet-switch-title"
+);
+const katakanaTitleEl = document.getElementById(
+	"katakana-alphabet-switch-title"
+);
+const rowCardEls = document.querySelectorAll(".row-card");
+const kanaProgressBarEls = document.querySelectorAll(".row-progress-completed");
+const kanaImageEls = document.querySelectorAll(".row-kana");
 
-    switchCheckbox.checked = !switchCheckbox.checked
+function booleanOfAlphabet(alphabetName) {
+	switch (alphabetName.toLowerCase()) {
+		case "katakana":
+			return true;
+		case "hiragana":
+			return false;
+		default:
+			throw new Error(`Invalid alphabet ${alphabetName}`);
+	}
+}
 
-    const titleEnabledClass = "enabled"
+function alphabetNameOf(boolean, capitalized = false) {
+	if (capitalized) {
+		return boolean ? "Katakana" : "Hiragana";
+	} else {
+		return boolean ? "katakana" : "hiragana";
+	}
+}
 
-    if(switchCheckbox.checked) {
-        play("alphabet_switch_katakana")
+function switchAlphabet() {
+	const alphabetBoolean = !switchCheckboxEl.checked;
+	const alphabetName = alphabetNameOf(alphabetBoolean);
+	switchCheckboxEl.checked = alphabetBoolean;
 
-        katakanaTitle.classList.add(titleEnabledClass)
-        hiraganaTitle.classList.remove(titleEnabledClass)
-        kanaImages.forEach(kana => kana.src = kana.src.replace("Hiragana", "Katakana"))
+	classUnless(hiraganaTitleEl, "enabled", alphabetBoolean);
+	classIf(katakanaTitleEl, "enabled", alphabetBoolean);
 
-        kanaProgressBars.forEach(progressBar => {
-            progressBar.classList.remove('hiragana')
-            progressBar.classList.add('katakana')
-        })
+	kanaProgressBarEls.forEach((progressBar) => {
+		classUnless(progressBar, "hiragana", alphabetBoolean);
+		classIf(progressBar, "katakana", alphabetBoolean);
+	});
 
-        currentAlphabet = "katakana"
-    } else {
-        play("alphabet_switch_hiragana")
+	rowCardEls.forEach((rowCard) => {
+		classUnless(rowCard, "hiragana", alphabetBoolean);
+		classIf(rowCard, "katakana", alphabetBoolean);
+	});
 
-        katakanaTitle.classList.remove(titleEnabledClass)
-        hiraganaTitle.classList.add(titleEnabledClass)
-        kanaImages.forEach(kana => kana.src = kana.src.replace("Katakana", "Hiragana"))
+	kanaImageEls.forEach(
+		(kana) =>
+			(kana.src = kana.src.replace(
+				alphabetNameOf(!alphabetBoolean, true),
+				alphabetNameOf(alphabetBoolean, true)
+			))
+	);
 
-        kanaProgressBars.forEach(progressBar => {
-            progressBar.classList.add('hiragana')
-            progressBar.classList.remove('katakana')
-        })
+	play(`alphabet_switch_${alphabetName}`);
+	currentAlphabet = alphabetName;
+	updatePetalScores();
+}
 
-        currentAlphabet = "hiragana"
-    }
-
-    updatePetalScores()
-})
-
+alphabetSwitchEl.addEventListener("click", switchAlphabet);
+katakanaTitleEl.addEventListener("click", () =>
+	switchAlphabet(booleanOfAlphabet("Katakana"))
+);
+hiraganaTitleEl.addEventListener("click", () =>
+	switchAlphabet(booleanOfAlphabet("Hiragana"))
+);
 
 /**************************************************************************************************
  *                                          Event sounds                                          *
  **************************************************************************************************/
-document.querySelectorAll(".row").forEach(row => {
-    row.addEventListener("mouseenter", event => play("hover"))
-    row.addEventListener("click", event => play("click"))
-})
-
+document.querySelectorAll(".row").forEach((row) => {
+	row.addEventListener("mouseenter", () => play("hover"));
+	row.addEventListener("click", () => play("click"));
+});
 
 /**************************************************************************************************
  *                                       Opening the lessons                                      *
  **************************************************************************************************/
- document.querySelectorAll(".row").forEach(row => {
-    row.addEventListener("click", event => {
-        let mouse = { x: event.clientX, y: event.clientY }
-        let transitionCircle = document.createElement("div")
-        transitionCircle.id = "transition-circle"
-        transitionCircle.classList.add(currentAlphabet)
-        transitionCircle.style.cssText = `left: ${mouse.x}px; top: ${mouse.y}px`
-        document.body.appendChild(transitionCircle)
+document.querySelectorAll(".row").forEach((row) => {
+	row.addEventListener("click", (event) => {
+		let mouse = { x: event.clientX, y: event.clientY };
+		let transitionCircle = document.createElement("div");
+		transitionCircle.id = "transition-circle";
+		transitionCircle.classList.add(currentAlphabet);
+		transitionCircle.style.cssText = `left: ${mouse.x}px; top: ${mouse.y}px`;
+		document.body.appendChild(transitionCircle);
 
-        setTimeout(() => window.location.href = "/learn/" + currentAlphabet + "/" + (row.id.split("-")[1] == '' ? "vowels" : row.id.split("-")[1]), 2000);
-    })
-})
+		setTimeout(
+			() =>
+				(window.location.href =
+					"/learn/" +
+					currentAlphabet +
+					"/" +
+					(row.id.split("-")[1] == ""
+						? "vowels"
+						: row.id.split("-")[1])),
+			2000
+		);
+	});
+});
 
-updatePetalScores()
+/**************************************************************************************************
+ *                                          Progress reset                                        *
+ **************************************************************************************************/
+
+document
+	.getElementById("progress-reset-button")
+	.addEventListener("click", () => {
+		progressResetContainerEl.classList.add("visible");
+		contentContainerEl.style.display = "none";
+	});
+
+document
+	.getElementById("reset-progress-button")
+	.addEventListener("click", () => {
+		for (let { letter } of rows) {
+			for (let alphabet of ["hiragana", "katakana"]) {
+				eraseCookie(`${alphabet}_${letter}`);
+			}
+		}
+
+		window.location.href = "/";
+	});
+
+document
+	.getElementById("keep-progress-button")
+	.addEventListener("click", () => {
+		progressResetContainerEl.classList.remove("visible");
+		contentContainerEl.style.display = null;
+	});
+
+updatePetalScores();
+
+setInterval(() => {
+	for (let i = 0; i < 5; i += 1) {
+		createPetal(
+			{
+				x: Math.random() * window.innerWidth,
+				y: -window.innerHeight / 8,
+			},
+			7,
+			currentAlphabet === "hiragana" ? "petal" : "leaf"
+		);
+	}
+}, 1500);
