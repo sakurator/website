c889e7b44983b2ab7e2d99a11b3d9402207edce7
Anatoly Frolov <contact@anafro.ru>
2023-11-26 14:11:13 +1000

    Format

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,30 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 var cookie = express.cookie;
-const { PrismaClient } = require('@prisma/client')
+const { PrismaClient } = require("@prisma/client");
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
+router.get("/", async function (req, res, next) {
+	const rows = await prisma.row.findMany({
+		select: {
+			name: true,
+			letter: true,
+			petalsToComplete: true,
+		},
+		where: {
+			alphabetId: {
+				equals: 1,
+			},
+		},
+	});
 
-router.get('/', async function(req, res, next) {
-  const rows = await prisma.rows.findMany({
-    select: {
-      name: true,
-      letter: true,
-      petals_to_complete: true,
-    },
-    where: {
-      alphabet_id: {
-        equals: 1,
-      }
-    }
-  })
-
-  res.render('index', { 
-    alphabet: "Hiragana",
-    title: 'Learn Japanese alphabets for free!',
-    rows: rows,
-    cookies: req.cookies,
-  });
+	res.render("index", {
+		alphabet: "Hiragana",
+		title: "Learn Japanese alphabets for free!",
+		rows: rows,
+		cookies: req.cookies,
+	});
 });
 
 module.exports = router;
