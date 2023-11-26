348fdb16cee47c0d0d19b9cb92416f4a1a10614c
Anatoly Frolov <contact@anafro.ru>
2023-11-26 13:57:25 +1000

    Add a row to the renderer

--- routes/learn.js
+++ routes/learn.js
@@ -1,61 +1,73 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const { PrismaClient } = require('@prisma/client')
+const { PrismaClient } = require("@prisma/client");
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-Object.defineProperty(String.prototype, 'capitalize', {
-    value: function() {
-        return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
-    },
-    enumerable: false
+Object.defineProperty(String.prototype, "capitalize", {
+	value: function () {
+		return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
+	},
+	enumerable: false,
 });
 
 /* GET users listing. */
-router.get('/:alphabet/:row', async function(req, res, next) {
-    let alphabet = req.params.alphabet.capitalize();
-    let letter = req.params.row.toLowerCase()
+router.get("/:alphabet/:row", async function (req, res, next) {
+	let alphabet = req.params.alphabet.capitalize();
+	let letter = req.params.row.toLowerCase();
 
-    if(letter == "vowels")
-        letter = ""
+	if (letter === "vowels") letter = "";
 
-    let rowLetters = await prisma.letters.findMany({
-        where: {
-            rows: {
-                alphabets: {
-                    name: {
-                        equals: alphabet,
-                    },
-                },
-                letter: {
-                    equals: letter,
-                },
-            },
-        },
-    })
+	let row = await prisma.row.findFirst({
+		where: {
+			alphabet: {
+				name: {
+					equals: alphabet,
+				},
+			},
+			letter: {
+				equals: letter,
+			},
+		},
+	});
 
-    let allLetters = await prisma.letters.findMany({
-        where: {
-            rows: {
-                alphabets: {
-                    name: {
-                        equals: alphabet
-                    },
-                },
-            },
-        },
-    })
+	let rowLetters = await prisma.letter.findMany({
+		where: {
+			row: {
+				alphabet: {
+					name: {
+						equals: alphabet,
+					},
+				},
+				letter: {
+					equals: letter,
+				},
+			},
+		},
+	});
 
-    if(rowLetters.length == 0)
-        return res.status(404) 
+	let allLetters = await prisma.letter.findMany({
+		where: {
+			row: {
+				alphabet: {
+					name: {
+						equals: alphabet,
+					},
+				},
+			},
+		},
+	});
 
-    res.render('learn', { 
-        title: "Learning the " + alphabet + " " + letter + " row",
-        rowLetters: rowLetters,
-        allLetters: allLetters,
-        alphabet: alphabet,
-        letter: letter,
-    });
+	if (rowLetters.length == 0) return res.status(404);
+
+	res.render("learn", {
+		title: "Learning the " + alphabet + " " + letter + " row",
+		rowLetters: rowLetters,
+		allLetters: allLetters,
+		alphabet: alphabet,
+		letter: letter,
+		row: row,
+	});
 });
 
 module.exports = router;
