670853c666b34ee019727ac8a8bb0d7e306df207
Anatoly Frolov (anafro) <anatolyfroloff@gmail.com>
2026-05-17 11:43:38 +1000

    refactor: migrate from cjs to esm

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var learnRouter = require("./routes/learn");
-var privacyPolicyRouter = require("./routes/privacy-policy");
-var termsAndConditionsOfUseRouter = require("./routes/terms-and-conditions-of-use");
+var indexRouter = require("./routes/index").default;
+var learnRouter = require("./routes/learn").default;
+var privacyPolicyRouter = require("./routes/privacy-policy").default;
+var termsAndConditionsOfUseRouter = require("./routes/terms-and-conditions-of-use").default;
 
 var app = express();
 
@@ -32,18 +32,18 @@ app.use("/terms-of-use", termsAndConditionsOfUseRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-	next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = {};
 
-	// render the error page
-	res.status(err.status || 500);
-	res.render("error");
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
 module.exports = app;
